4188d16efa9cfad0fcfbbf6b71ca2bf63fc7d93e
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-01-25 23:01:07 +0000

    Class4_lab 2

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,22 @@
 # ruby 2-conditionals.rb
 
 # Booleans
-
+temp = 32
+precip = 100
 # Boolean Expressions
 
 # If Conditional Logic
-
+if temp > 60 && precip < 60
+    puts "Nice!"
 # If/Else Conditional Logic
+elsif temp > 60 && precip == 0
+    puts "Very Nice"
+
 
 # Elsif Conditional Logic
+else 
+    puts "worst"
+
+end
 
 # Combining Expressions
